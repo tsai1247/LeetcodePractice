b5dc9ccc62aba2d93ed60d5c841c393fb69a873f
tsai1247 <jeff29cc91079@gmail.com>
2023-03-14 15:17:24 +0800

    [Update] problem 000203

---
+++ 000203- Remove Linked List Elements/Solution.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+#include "../ListNode.h"
+using namespace std;
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next
+(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val
+(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == NULL)    return head;
+        ListNode* cur = head->next, *prev = head;
+        while(cur!=NULL)
+        {
+            if(cur->val == val)
+            {
+                cur = cur->next;
+                prev->next = cur;
+            }
+            else
+            {
+                cur = cur->next;
+                prev = prev->next;
+            }
+        }
+        
+        if(head->val == val)
+            head = head->next;
+
+        return head;
+    }
+
+    void test() {
+        assert( 
+            Equals(
+                removeElements( make_ListNode(*(new vector<int>({1,2,6,3,4,5,6}))), 6 ), 
+                make_ListNode(*(new vector<int>({1, 2, 3, 4, 5}))) 
+            )
+        );
+        assert( 
+            Equals(
+                removeElements( make_ListNode(*(new vector<int>({}))), 5 ), 
+                make_ListNode(*(new vector<int>({}))) 
+            )
+        );
+        
+        assert( 
+            Equals(
+                removeElements( make_ListNode(*(new vector<int>({1}))), 1 ), 
+                make_ListNode(*(new vector<int>({}))) 
+            )
+        );
+
+        assert( 
+            Equals(
+                removeElements( make_ListNode(*(new vector<int>({1}))), 6 ), 
+                make_ListNode(*(new vector<int>({1}))) 
+            )
+        );
+        
+        assert( 
+            Equals(
+                removeElements( make_ListNode(*(new vector<int>({6, 6, 6, 5, 6, 6, 6}))), 6 ), 
+                make_ListNode(*(new vector<int>({5}))) 
+            )
+        );
+
+        cout<<"test succeeded.\n";
+    }
+};
+
+int main()
+{
+    Solution().test();
+}
