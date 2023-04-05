a5b26b711f67b29e71ced62275a93daeffe6d4d3
tsai1247 <jeff29cc91079@gmail.com>
2023-04-06 07:31:19 +0800

    [Update] problem 001491

---
+++ Algorithms/001491- Average Salary Excluding the Minimum and Maximum Salary/Solution.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time complexity: O(   )
+// Space complexity: O(   )
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        int sum = 0, minimum = INT_MAX, maximum = 0;
+        for(auto& num : salary)
+        {
+            sum += num;
+            minimum = min(minimum, num);
+            maximum = max(maximum, num);
+        }
+        return (sum - minimum - maximum)*1.0 / (salary.size()-2);
+    }
+
+    void test() {
+        assert(average( *(new vector<int>( {1000, 2000, 3000} )) ) == 2000 );
+        assert(average( *(new vector<int>( {4000, 3000, 1000, 2000} )) ) == 2500 );
+        cout<<"test succeeded.\n";
+    }
+};
+
+int main()
+{
+    Solution().test();
+}
