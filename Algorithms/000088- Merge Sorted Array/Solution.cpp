753b533cab652740b49f6d80a0903eac09f2079e
tsai1247 <jeff29cc91079@gmail.com>
2025-01-21 22:50:57 +0800

    feat: optimize problem 000088

--- Algorithms/000088- Merge Sorted Array/Solution.cpp
+++ Algorithms/000088- Merge Sorted Array/Solution.cpp
@@ -6,28 +6,20 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m-1, j = n-1, cur = m+n-1;
-        while(i>=0 || j>=0)
+        int index = nums1.size() - 1;
+        while(n > 0)
         {
-            if(i < 0) // put nums2[j] into answer
+            if(m == 0 || nums1[m-1] < nums2[n-1])
             {
-                nums1[cur--] = nums2[j--];
-            }
-            else if(j < 0) // put nums1[i] into answer
-            {
-                nums1[cur--] = nums1[i--];
+                nums1[index] = nums2[n-1];
+                n--;
             }
             else
             {
-                if(nums1[i] > nums2[j])
-                {
-                    nums1[cur--] = nums1[i--];
-                }
-                else
-                {
-                    nums1[cur--] = nums2[j--];
-                }
+                nums1[index] = nums1[m-1];
+                m--;
             }
+            index--;
         }
     }
 
@@ -35,15 +27,15 @@ public:
     void test() {
 		vector<int> v1;
 
-		v1 = *(new vector<int>( {1,2,3,0,0,0} ));
+		v1 = {1,2,3,0,0,0};
 		merge( v1, 3, *(new vector<int>( {2, 5, 6} )), 3 );
         assert( v1 ==  *(new vector<int>( {1,2,2,3,5,6} )));
         
-		v1 = *(new vector<int>( {1} ));
+		v1 = {1};
 		merge( v1, 1, *(new vector<int>( {} )), 0 );
         assert( v1 ==  *(new vector<int>( {1} )));
 		
-		v1 = *(new vector<int>( {0} ));
+		v1 = {0};
 		merge( v1, 0, *(new vector<int>( {1} )), 1 );
         assert( v1 ==  *(new vector<int>( {1} )));
 		
